41f2e650b6fd6c737da72a80222027421a493d84
Karthikeyan Natarajan <karthiknitt@gmail.com>
2024-07-16 00:03:17 +0530

    Full code after L14, MERN WorkoutBuddy App

--- mern/backend/controllers/workoutController.js
+++ mern/backend/controllers/workoutController.js
@@ -25,6 +25,17 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   console.log('POST A new workout');
   const { title, reps, load } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) emptyFields.push(title);
+  if (!load) emptyFields.push(load);
+  if (!reps) emptyFields.push(reps);
+
+  if (emptyFields.length > 0)
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all required fields', emptyFields });
   //add doc to db
   try {
     const workout = await Workout.create({ title, reps, load });
