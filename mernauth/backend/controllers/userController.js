98e8f84526521cf6f4be8a3241f6c11eb2178146
Karthikeyan Natarajan <karthiknitt@gmail.com>
2024-07-16 19:40:17 +0530

    L6 ON AUTH TUTORIAL

--- mernauth/backend/controllers/userController.js
+++ mernauth/backend/controllers/userController.js
@@ -1,5 +1,10 @@
 //import the mongoose schema model from the userModel to acess the users collection from MongoDB
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken'; //import jwt from pkg
+
+const create_token = _id => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+};
 
 //login user controller function logic
 const loginUser = async (req, res) => {
@@ -12,8 +17,11 @@ const signupUser = async (req, res) => {
   try {
     //get a single instance of object user submitted by invoking the signup function on the imported User object collection
     const user = await User.signup(email, password);
-    //return a status of 200 ok and an email,single user object
-    res.status(200).json({ email, user });
+
+    // create a token to be sent back using the inbuilt _id
+    const token = create_token(user._id);
+    //return a status of 200 ok and an email,jwt token
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message }); //display error status and the messgae from the throw error function of signup static function
   }
