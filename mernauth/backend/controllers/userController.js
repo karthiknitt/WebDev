b2ab91b24a5ecba762b80fd079d1e4856df3302b
Karthikeyan Natarajan <karthiknitt@gmail.com>
2024-07-16 17:52:01 +0530

    L3 ON AUTH TUTORIAL

--- mernauth/backend/controllers/userController.js
+++ mernauth/backend/controllers/userController.js
@@ -7,7 +7,16 @@ const loginUser = async (req, res) => {
 };
 //signup user controller function logic
 const signupUser = async (req, res) => {
-  res.json({ msg: 'User Signed Up' });
+  //Get a copy of of email and password submitted from the req's body and destructure
+  const { email, password } = req.body;
+  try {
+    //get a single instance of object user submitted by invoking the signup function on the imported User object collection
+    const user = await User.signup(email, password);
+    //return a status of 200 ok and an email,single user object
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message }); //display error status and the messgae from the throw error function of signup static function
+  }
 };
 
 export { loginUser, signupUser }; //export both functions for use in users.js
--- mernauth/backend/models/userModel.js
+++ mernauth/backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'; //Mongoose is ORM for MongoDB
 const Schema = mongoose.Schema; //Define schema as a Mongoose Schema
+import bcrypt from 'bcrypt'; //Tool for encrypting passwords
 
 //Define the actual schema for the user. Email is unique. Both email and passwords are required fields.
 
@@ -18,4 +19,24 @@ const userSchema = new Schema(
   { timestamps: true } //adds the createdAt variable in the MongoDB collection
 );
 
+//MongoDB/Mongoose lets us create our own methods in the model, here we create one called signup
+
+userSchema.statics.signup = async function (email, password) {
+  //check if email already exists
+  //User collection is exported only at the last line of this file and hence it is not available. So we use the this keyword
+  //Also note that we are using the regular function type declaration using function keyword for this operator to work. If we used an arrow fucntion this won't work
+
+  const email_exists = await this.findOne({ email }); //async code awaiting db action
+  if (email_exists) {
+    throw Error('Email already in use.'); //can't send res.status now so just throw an error now as User isnt available yet
+  }
+  //salt is a random string added to plain passwords so that ven same passwords wont have the same hash
+  const salt = await bcrypt.genSalt();
+  //send salt and passwords to bcrypt hash to generate the new hashed password and write only the hashed password to db
+  const hash = await bcrypt.hash(password, salt);
+  //create a new mongodb document here
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 export default mongoose.model('User', userSchema); //Export the model as a mongoose schema for use in the usercontroller.js
