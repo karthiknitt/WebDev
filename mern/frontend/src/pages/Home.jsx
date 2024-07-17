8795f3a1dada371cc206fcd1ebb9a759c609059c
Karthikeyan Natarajan <karthiknitt@gmail.com>
2024-07-17 12:09:01 +0530

    Final without auth

--- mern/frontend/src/pages/Home.jsx
+++ mern/frontend/src/pages/Home.jsx
@@ -1,32 +1,37 @@
-import React from 'react';
 import { useEffect } from 'react';
-import WorkoutDetails from '../components/WorkoutDetails';
-import WorkoutForm from '../components/WorkoutForm';
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
-function Home() {
+// components
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+
+const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch('/api/workouts');
       const json = await response.json();
+
       if (response.ok) {
         dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
     };
+
     fetchWorkouts();
   }, [dispatch]);
+
   return (
     <div className="Home">
       <div className="workouts">
         {workouts &&
           workouts.map(workout => (
-            <WorkoutDetails key={workout._id} workout={workout} />
+            <WorkoutDetails workout={workout} key={workout._id} />
           ))}
       </div>
       <WorkoutForm />
     </div>
   );
-}
+};
 
 export default Home;
