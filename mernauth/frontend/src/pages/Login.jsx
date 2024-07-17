b5139c23b44fcee8ac5dbf00d28434e26a377161
Karthikeyan Natarajan <karthiknitt@gmail.com>
2024-07-17 17:27:15 +0530

    L12 ON AUTH TUTORIAL

--- mernauth/frontend/src/pages/Login.jsx
+++ mernauth/frontend/src/pages/Login.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  function handleSubmit(e) {
+  const { login, error, isLoading } = useLogin();
+
+  const handleSubmit = async e => {
     e.preventDefault();
-  }
+
+    await login(email, password);
+  };
+
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log In</h3>
-      <label>Email:</label>
+
+      <label>Email address:</label>
       <input
         type="email"
-        onChange={e => {
-          setEmail(e.target.value);
-        }}
+        onChange={e => setEmail(e.target.value)}
         value={email}
-      ></input>
-
+      />
       <label>Password:</label>
       <input
         type="password"
-        onChange={e => {
-          setPassword(e.target.value);
-        }}
+        onChange={e => setPassword(e.target.value)}
         value={password}
-      ></input>
-      <button>Log In</button>
+      />
+
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
